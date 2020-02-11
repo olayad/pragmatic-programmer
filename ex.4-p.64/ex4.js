6ed325bb9d4b8de9e4546785742d67801f3fcee8
Daniel Olaya <d_olaya@hotmail.com>
2020-02-10 21:39:55 -0800

    WIP: Stubs for processing user input

--- ex.4-p.64/ex4.js
+++ ex.4-p.64/ex4.js
@@ -5,15 +5,40 @@ var rl = readline.createInterface({
  output: process.stdout
 });
 
+var input = {
+    "P": {"hasArg": true},
+    "D": {"hasArg": false},
+    "U": {"hasArg": false},
+    "W": {"hasArg": true},
+    "N": {"hasArg": true},
+    "E": {"hasArg": true},
+    "S": {"hasArg": true}
+};
+
+
 var waitForUserInput = function() {
   rl.question("Please give an instruction: ", function(answer) {
     if (answer == "exit"){
         rl.close();
     } else {
-        console.log("you said: ", answer);
+        console.log("user said: ", answer);
+        processInput(answer);
         waitForUserInput();
     }
   });
 };
 
 waitForUserInput();
+
+function processInput(input) {
+    // TODO: input should be converted to upper case before split
+    // splitted = input.split(" ");
+    // console.log("typeof splitted: ", typeof splitted[0])
+    // console.log("splitted: ", splitted[0]);
+    // console.log("type splitted :", typeof input[splitted[0]]);
+    if (input[splitted[0].toUpperCase()]){
+//         console.log("instruction found!");
+//     } else{
+//         console.log(" could not find instruction :(");
+//     }
+// }
