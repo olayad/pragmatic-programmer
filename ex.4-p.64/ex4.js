de810e53f7ada42abadf5aba9f4c01d01f84ca1c
Daniel Olaya <d_olaya@hotmail.com>
2020-02-12 17:51:33 -0800

    Added final logic for running command with and without args

--- ex.4-p.64/ex4.js
+++ ex.4-p.64/ex4.js
@@ -5,20 +5,28 @@ var rl = readline.createInterface({
  output: process.stdout
 });
 
+var penDown = function(step){ console.log("[INFO] doing pen down..."+step) };
+var down = function(){ console.log("[INFO] doing down...") };
+var west = function(step){ console.log("[INFO] doing west..."+step) };
+var north = function(step){ console.log("[INFO] doing north..."+step) };
+var east = function(step){ console.log("[INFO] doing east..."+step) };
+var south = function(step){ console.log("[INFO] doing south..."+step) };
+var penUp = function(){ console.log("[INFO] doing pen up...") };
+
 var instruction = {
-    "P": {"hasArg": true},
-    "D": {"hasArg": false},
-    "U": {"hasArg": false},
-    "W": {"hasArg": true},
-    "N": {"hasArg": true},
-    "E": {"hasArg": true},
-    "S": {"hasArg": true}
+    "P": {"hasArg": true, "action": penDown},
+    "D": {"hasArg": false, "action": down},
+    "U": {"hasArg": false, "action": penUp},
+    "W": {"hasArg": true, "action": west},
+    "N": {"hasArg": true, "action": north},
+    "E": {"hasArg": true, "action": east},
+    "S": {"hasArg": true, "action": south}
 };
 
 
 var waitForUserInput = function() {
   rl.question("Please give an instruction: ", function(answer) {
-    if (answer == "exit"){
+    if (answer === "exit"){
         rl.close();
     } else {
         processInput(answer);
@@ -27,19 +35,55 @@ var waitForUserInput = function() {
   });
 };
 
-waitForUserInput();
+const INVALID = 0;
+const VALID_NO_ARGS = 1;
+const VALID_WITH_ARGS = 2;
+
 
 function processInput(input) {
-    // TODO: input should be converted to upper case before split
     input = input.toUpperCase().split(" ");
-    if (instruction[input[0]]){
-        if (instruction[input[0]]["hasArg"]) {
-            console.log("Found and has args")
-        } else{
-            console.log("Found, NO args")
-        }
+    let instuctionType = isValidInstruction(input);
+    if (instuctionType){
+        executeInstruction(input, instuctionType)
     } else{
-        console.log(" could not find instruction :(");
+        console.log("[Error] Could not execute instruction :( ");
     }
 }
 
+
+function isValidInstruction(input){
+    if (instructionExists(input)){     // Check it exists
+        if (instructionNeedArgs(input)){    //Check it has args needed
+            if (input[1] === undefined){
+                console.log('[Error] Instruction should have included argument');
+                return INVALID;
+            }else{
+                return VALID_WITH_ARGS;
+            }
+        } else{
+            console.log('[Info] Valid instruction; no args needed]!!');
+            return VALID_NO_ARGS;
+        }
+    }
+}
+
+function instructionExists(input) {
+    return instruction[input[0]] ? true : false;
+}
+
+function instructionNeedArgs(input) {
+    return instruction[input[0]]["hasArg"] ? true : false;
+}
+
+function executeInstruction(input, arg){
+    if (arg === VALID_NO_ARGS){
+        instruction[input[0]]["action"]();
+    }
+    else if (arg === VALID_WITH_ARGS){
+        instruction[input[0]]["action"](input[1]);
+    } else {
+        console.log("[ERROR] executeInstruction - I don't know what I am being asked");
+    }
+}
+
+waitForUserInput();
